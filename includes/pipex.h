c64c7af265214ecd113ebb6e5aa12f93e350e66e
Valentin Durand <vdurand@z2r11p6.42lyon.fr>
2025-01-24 15:50:20 +0100

    Here doc finished

--- includes/pipex.h
+++ includes/pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: val <val@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: vdurand <vdurand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 16:46:50 by val               #+#    #+#             */
-/*   Updated: 2025/01/22 17:03:44 by val              ###   ########.fr       */
+/*   Updated: 2025/01/24 15:25:26 by vdurand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,10 @@
 # include "unistd.h"
 # include <sys/types.h>
 # include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+//
+# define TEMP_PATH	"./.tmp_heredoc"
+# define ERROR_USAGE	"Error: Not enough arguments. Usage: pipex <argument1> <argument2> ...\n"
+//
 #endif
--- src/main.c
+++ src/main.c
@@ -3,23 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: val <val@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: vdurand <vdurand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 16:41:21 by val               #+#    #+#             */
-/*   Updated: 2025/01/22 17:35:51 by val              ###   ########.fr       */
+/*   Updated: 2025/01/24 15:39:03 by vdurand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+int	open_file(char *path, int mode)
+{
+	if (mode == 0)
+		return (open(path, O_RDONLY));
+	if (mode == 1)
+		return (open(path, O_RDWR | O_APPEND | O_CREAT, 0777));
+	if (mode == 2)
+		return (open(path, O_RDWR | O_TRUNC | O_CREAT, 0777));
+	return (-1);
+}
+
+char	*ft_fstrjoin(char *s1, const char *s2)
+{
+	char	*result;
+
+	result = ft_strjoin(s1, s2);
+	free(s1);
+	return (result);
+}
+
+int	error(void)
+{
+	perror("Failed");
+	return (0);
+}
+
+int	read_heredoc(char *LIMITER)
+{
+	char	heredoc[1024];
+	char	*search;
+	size_t	readed;
+	int		file;
+	
+	if (access("./", R_OK | W_OK) != 0)
+		return (error());
+	file = open_file(TEMP_PATH, 2);
+	if (!file)
+		return (error());
+	while (1)
+	{
+		ft_putstr_fd("\e[32;47mheredoc >\e[0m ", 2);
+		readed = read(STDIN_FILENO, heredoc, 1023);
+		heredoc[readed] = '\0';
+		search = ft_strnstr(heredoc, LIMITER, readed);
+		if (search)
+		{
+			*(search--) = '\0';
+			ft_putstr_fd(heredoc, file);
+			break ;
+		}
+		ft_putstr_fd(heredoc, file);
+	}
+	return (close(file), 1);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	(void) argc;
-	(void) argv;
-	int i = 0;
-    while (envp[i] != NULL) {
-        ft_printf("Variable d'environnement %d : %s\n", i, envp[i]);
-        i++;
-    }
-    return 0;
+	(void) envp;
+	if (argc < 5)
+		return (ft_putstr_fd(ERROR_USAGE, 2), EXIT_FAILURE);
+	read_heredoc(argv[2]);
+	unlink(TEMP_PATH);
+    return (EXIT_SUCCESS);
 }
